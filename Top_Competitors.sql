68fd01774f3669c1cf2a0b65cbcb5d98f00b0ce5
Michael Schmidek <88997687+MichaelSchmidek@users.noreply.github.com>
2022-11-19 15:39:15 -0800

    Update Top_Competitors.sql

--- Top_Competitors.sql
+++ Top_Competitors.sql
@@ -11,9 +11,9 @@ https://www.hackerrank.com/challenges/full-score/problem?isFullScreen=true
 SELECT S.hacker_id, H.name
 FROM Submissions AS S
 JOIN Hackers AS H ON H.hacker_id = S.hacker_id
-JOIN Challenges as C ON C.challenge_id = S.Challenge_id
+JOIN Challenges AS C ON C.challenge_id = S.Challenge_id
 JOIN Difficulty AS D ON D.difficulty_Level = C.difficulty_level
 WHERE S.score = D.score
 GROUP BY H.hacker_id, H.name
-Having COUNT(S.Challenge_id) >1
+HAVING COUNT(S.Challenge_id) >1
 ORDER BY COUNT(S.Challenge_id) DESC, H.hacker_id ASC
