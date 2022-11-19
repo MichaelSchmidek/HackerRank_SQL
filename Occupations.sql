09bf8d08264263bd5b4943ffed05e87e3e21ad4c
Michael Schmidek <88997687+MichaelSchmidek@users.noreply.github.com>
2022-11-19 14:19:12 -0800

    Update Occupations.sql

--- Occupations.sql
+++ Occupations.sql
@@ -3,6 +3,9 @@
 Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. 
 The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
 Note: Print NULL when there are no more names corresponding to an occupation.
+
+Link to problem:
+https://www.hackerrank.com/challenges/occupations/problem?isFullScreen=true
 */
 
 SET @d = 0, @p = 0, @s = 0, @a = 0;
