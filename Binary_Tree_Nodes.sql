4b9d74fce6e0e7d0cfe69b8ac42a5f36da5de57c
Michael Schmidek <88997687+MichaelSchmidek@users.noreply.github.com>
2022-11-20 15:54:48 -0800

    Create Binary_Tree_Nodes.sql

---
+++ Binary_Tree_Nodes.sql
@@ -0,0 +1,15 @@
+/*
+You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+
+Link to Problem:
+https://www.hackerrank.com/challenges/binary-search-tree-1/problem?isFullScreen=true
+*/
+
+SELECT N, IF(P IS NULL, 'Root',IF((SELECT COUNT(*) FROM BST WHERE P = B.N) > 0, 'Inner', 'Leaf'))
+FROM BST AS B
+ORDER BY N;
